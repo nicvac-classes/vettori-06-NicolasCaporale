b23a0ab80a59728e6322733e0594ad769fe09076
NicolasCaporale <nicolas.caporale@marconibari.edu.it>
2024-11-21 21:04:14 +0000

    fds

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+  srand(0);
+  int n,i,itempomin;
+  do 
+  {
+    cout << "Ins numero atleti: ";
+    cin >> n;
+  }while(n <= 0);
+  vector<string> nomi(n);
+  vector<string> nazionali(n);
+  vector<float> tempi(n);
+  
+  for(i = 0; i < n; i++) 
+  {
+    cout << "Ins nome atleta & nazionale: ";
+    cin >> nomi[i] >> nazionali[i];
+    tempi[i] = (1000 + rand() % 501) / 100.0;
+  }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+  for(i = 0; i < n; i++) 
+  {
+    cout << "Nome: " << nomi[i] << ", nazionale: " << nazionali[i] << ", tempo: " << tempi[i] << endl;
+  }
+  itempomin = 0;
+  for(i = 0; i < tempi.size(); i++) 
+  {
+    if(tempi[itempomin] > tempi[i]) 
+    {
+      itempomin = i;
+    }
+  }
+
+  cout << "Il vincitore è: " << nomi[itempomin] << ", nazionale: " << nazionali[itempomin] << ", tempo: " << tempi[itempomin] << endl;
+}
